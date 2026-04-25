57e9fb36cadae0aa2554ca3155dad37a7f8dc30d
princejetro <adegbuyijephthah@gmail.com>
2026-04-25 18:35:50 +0100

    runtime error

--- brainbattle/settings.py
+++ brainbattle/settings.py
@@ -79,23 +79,23 @@ WSGI_APPLICATION = 'brainbattle.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql',
-         'NAME': 'postgres',
-         'HOST' : "aws-1-eu-north-1.pooler.supabase.com",
-         "PORT" : "6543",
-         'USER' : "postgres.esucgagleqfveepkstjk",
-         'PASSWORD' : "gh7pYc72baDNyFyR",
-     }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.postgresql',
+#          'NAME': 'postgres',
+#          'HOST' : "aws-1-eu-north-1.pooler.supabase.com",
+#          "PORT" : "6543",
+#          'USER' : "postgres.esucgagleqfveepkstjk",
+#          'PASSWORD' : "gh7pYc72baDNyFyR",
+#      }
+# }
 
 
 
